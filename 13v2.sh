914928d146cc1b45ce3007c21ac14d5b55278ad3
ZAABOUL HAYTHAM <zaaboulhaytham@gmail.com>
2025-12-11 14:27:17 +0100

    Refactor OSPF configuration to use vtysh commands

--- 13v2.sh
+++ 13v2.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 set -e
 
-# Mise en UP des interfaces
+# Interfaces
 ip link set dev enp1s0f0 up
 ip link set dev enp1s0f1 up
-ip a a 120.0.65.3/24 dev enp1s0f0
-ip a a 120.0.64.1/24 dev enp1s0f1
+ip a a 120.0.65.3/24 dev enp1s0f0 || true
+ip a a 120.0.64.1/24 dev enp1s0f1 || true
 
-# Activer le routage IPv4
+# Routage
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# Copier le fichier daemons
+# daemons
 cp ./daemons /etc/quagga/daemons
 chown quagga:quagga /etc/quagga/daemons
 chmod 640 /etc/quagga/daemons
-
-# Redémarrer Quagga
 systemctl restart quagga
 
-# Config OSPF SANS EXPECT
-vtysh <<EOF
-configure terminal
-router ospf
- network 120.0.65.0/24 area 1
- network 120.0.64.0/24 area 2
- router-id 13.13.13.13
-end
-write
-EOF
+# Config OSPF SANS vtysh interactif
+vtysh \
+  -c "configure terminal" \
+  -c "router ospf" \
+  -c "network 120.0.65.0/24 area 1" \
+  -c "network 120.0.64.0/24 area 2" \
+  -c "router-id 13.13.13.13" \
+  -c "end" \
+  -c "write"
